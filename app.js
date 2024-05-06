9e3314656f23bf68bc55e1584a6c097eb6482ab2
hama <75150231+hamanyann@users.noreply.github.com>
2024-05-06 22:20:53 +0900

    コミットメッセージをここに入力してください

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
 (() => {
-    // 計算式
-    const $result = 1 + 3;
-
-    // 計算結果を表示
-    document.getElementById("total").textContent = $result;
 
 
+    // ボタンを抽出
+    const buttons = document.querySelectorAll('button');
+    let a = "";
+    let b = "";
+    let result = "";
 
-   
+    function number(num) {
+        b += num;
+        document.getElementById("total").textContent = b;
+    }
+
+    function calculate() {
+
+        let c = document.getElementById('total').textContent;
+        result = eval(c);
+        document.getElementById('total').textContent = result;
+    }
+
+
+
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            // クリックされたボタンの id を取得し、コンソールに出力
+            console.log(button.id);
+
+            if (button.id === "clear") {
+                document.getElementById("total").textContent = 0;
+                document.getElementById("memory").textContent = 0;
+                b = "";
+
+            } else if (button.id === "is") {
+                calculate();
+            } else if (button.id === "save") {
+
+                document.getElementById("memory").textContent = result;
+
+            } else if (button.id === "call") {
+                number(document.getElementById("memory").textContent);
+
+            }
+
+            else {
+                number(button.id);
+            }
+
+
+        });
+    });
+
+
+
+
+
 
 
 
